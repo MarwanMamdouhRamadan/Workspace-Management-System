d68aa30f572402434ca52a295c0ed7c07ac83ff9
MarwanMamdouhRamadan <marwan.mamdoh.ramdan@gmail.com>
2026-01-23 10:09:40 +0200

    Add IGenricRepo and IlookupServiecs

---
+++ Workspace.Infrastructure/Repositories/Interfaces/IGenricRepo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workspace.Infrastructure.Repositories.Interfaces
+{
+    public interface IGenricRepo<T> where T : class
+    {
+        Task<IEnumerable<T>> getAll();
+        Task<T> getById(int id);
+        Task add(T entity);
+        void update(T entity);
+        void delete(T entity);
+    }
+}
---
+++ Workspace.Infrastructure/Repositories/Interfaces/ILookupService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Workspace.Application.DTOs.request;
+
+namespace Workspace.Infrastructure.Repositories.Interfaces
+{
+    public interface ILookupService
+    {
+        IEnumerable<StatusTypeDto> GetAllCachedStatusTypes();
+        void RefreshCache();
+    }
+}
