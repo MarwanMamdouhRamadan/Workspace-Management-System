cb30ce851619d0dd0557c96725541013240a2ed6
MarwanMamdouhRamadan <marwan.mamdoh.ramdan@gmail.com>
2026-01-23 10:08:43 +0200

    Adding StatusTpye dto

---
+++ Workspace.Application/DTOs/request/StatusTypeDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workspace.Application.DTOs.request
+{
+    public class StatusTypeDto
+    {
+        [Required(ErrorMessage ="please enter the type name")]
+        public string TypeName { get; set; }
+    }
+}
