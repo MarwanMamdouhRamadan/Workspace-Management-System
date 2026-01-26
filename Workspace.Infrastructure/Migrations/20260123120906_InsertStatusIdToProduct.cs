f21a54d81f7e79f647e2021d37cca06812032b15
MarwanMamdouhRamadan <marwan.mamdoh.ramdan@gmail.com>
2026-01-26 18:27:49 +0200

    Add new migration files

---
+++ Workspace.Infrastructure/Migrations/20260123120906_InsertStatusIdToProduct.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Workspace.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InsertStatusIdToProduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "StatusId",
+                table: "TbProduct",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TbProduct_StatusId",
+                table: "TbProduct",
+                column: "StatusId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TbProduct_TbStatus_StatusId",
+                table: "TbProduct",
+                column: "StatusId",
+                principalTable: "TbStatus",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TbProduct_TbStatus_StatusId",
+                table: "TbProduct");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TbProduct_StatusId",
+                table: "TbProduct");
+
+            migrationBuilder.DropColumn(
+                name: "StatusId",
+                table: "TbProduct");
+        }
+    }
+}
---
+++ Workspace.Infrastructure/Migrations/20260125145902_MakeStatusIdNotUnique.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Workspace.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeStatusIdNotUnique : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_TbProduct_StatusId",
+                table: "TbProduct");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TbProduct_StatusId",
+                table: "TbProduct",
+                column: "StatusId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_TbProduct_StatusId",
+                table: "TbProduct");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TbProduct_StatusId",
+                table: "TbProduct",
+                column: "StatusId",
+                unique: true);
+        }
+    }
+}
