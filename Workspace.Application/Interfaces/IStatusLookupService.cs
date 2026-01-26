64587994b7fd2438405cee4a2d86b85ca4211301
MarwanMamdouhRamadan <marwan.mamdoh.ramdan@gmail.com>
2026-01-26 14:52:08 +0200

    Refactor: merge status lookup services into a unified LookupService and clean up old files

--- Workspace.Application/Interfaces/IStatusLookupService.cs
+++ Workspace.Application/Interfaces/IStatusLookupService.cs
@@ -3,14 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Workspace.Application.DTOs.request;
-using Workspace.Application.DTOs.response;
 
-namespace Workspace.Infrastructure.Repositories.Interfaces
+namespace Workspace.Application.Interfaces
 {
-    public interface IStatusLookupService
+    internal class IStatusLookupService
     {
-        IEnumerable<StatusDtoResponse> GetAllCachedStatusTypes();
-        void RefreshCache();
     }
 }
